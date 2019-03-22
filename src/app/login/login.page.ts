5bad34dbabf54327fd4509a3d6a3902f54dff822
Unknown <manualsplash@gmail.com>
2019-03-22 22:29:18 +0530

    Add login page basic functionality

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import swal from 'sweetalert'
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      userEmail: ['', Validators.required],
+      userPassword: ['', Validators.required]
+    });
+  }
+  public hasError(controlName: string, errorName: string) {
+    return this.loginForm.controls[controlName].hasError(errorName);
   }
 
+  loginAction(){
+    swal('sdjashdaksjd');
+  }
 }
