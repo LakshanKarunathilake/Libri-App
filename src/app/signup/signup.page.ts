6858a8edda23d1f9a22c9d8073e6977007ab0c44
Unknown <manualsplash@gmail.com>
2019-03-25 00:38:27 +0530

    Initial view for signup

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss'],
+  styleUrls: ['./signup.page.scss']
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
+  signupForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.signupForm = this.fb.group({
+      displayName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      telephoneNumber: ['', Validators.required]
+    });
   }
 
+  public hasError(controlName: string, errorName: string) {
+    return this.signupForm.controls[controlName].hasError(errorName);
+  }
 }
