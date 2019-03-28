8923b6264bec489725d53bf1b03ffe7be8693fda
Unknown <manualsplash@gmail.com>
2019-03-28 23:21:58 +0530

    Import reactive forms module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
 import { IonicModule } from '@ionic/angular';
@@ -20,7 +20,8 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
-    MatInputModule
+    MatInputModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginPage]
 })
