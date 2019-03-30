00006a7764acb969083b1b054d8c6a1eb53b12f1
Unknown <manualsplash@gmail.com>
2019-03-30 23:18:51 +0530

    Add firebase config to environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD2ubSfF7xjL6cVKzO038c18MSf_dY1knU',
+    authDomain: 'libri-9746e.firebaseapp.com',
+    databaseURL: 'https://libri-9746e.firebaseio.com',
+    projectId: 'libri-9746e',
+    storageBucket: 'libri-9746e.appspot.com',
+    messagingSenderId: '893322827741'
+  }
 };
 
 /*
