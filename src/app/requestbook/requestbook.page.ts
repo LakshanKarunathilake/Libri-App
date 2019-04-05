350f3d8aafb1f8392639190e067be88a43e72aef
Unknown <manualsplash@gmail.com>
2019-04-05 22:57:43 +0530

    Create requestbook page

---
+++ src/app/requestbook/requestbook.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requestbook',
+  templateUrl: './requestbook.page.html',
+  styleUrls: ['./requestbook.page.scss'],
+})
+export class RequestbookPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
