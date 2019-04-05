f127c7bb87b1896dd86d6f12c16b413803adb644
Unknown <manualsplash@gmail.com>
2019-04-05 22:58:28 +0530

    Add authentication validation and sweet alert for login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import swal from 'sweetalert';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,7 +10,7 @@ import swal from 'sweetalert';
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private afa: AngularFireAuth, private router: Router) {}
   ngOnInit() {
     this.loginForm = this.fb.group({
       userEmail: ['', Validators.required],
@@ -20,7 +22,22 @@ export class LoginPage implements OnInit {
   }
 
   loginAction() {
-    swal(this.loginForm.value);
+    const userEmail = this.loginForm.controls['userEmail'].value;
+    const userPassword = this.loginForm.controls['userPassword'].value;
+    try {
+      this.afa.auth
+        .signInWithEmailAndPassword(userEmail, userPassword)
+        .then(() => {
+          swal('Welcome back!', '', 'success');
+          this.router.navigateByUrl('home');
+        })
+        .catch(error => {
+          console.log('error :', error, error.message);
+          this.previewErrorMessage(error);
+        });
+    } catch (error) {
+      console.log('error Occured  :', error, error.message);
+    }
   }
 
   getErrorMessage = (controller: string) => {
@@ -36,4 +53,28 @@ export class LoginPage implements OnInit {
   checkFormControlIsValid(control: string) {
     return this.loginForm.controls[control].invalid;
   }
+
+  previewErrorMessage = ({ code, message }) => {
+    const swalProps = { title: '', type: 'error', description: '' };
+    switch (code) {
+      case 'network-request-failed':
+        swalProps.description = 'Check your internet Connection';
+        swalProps.title = 'Connection Failure';
+        break;
+      case 'auth/user-disabled':
+        swalProps.description = 'Your connection is disabled please contact authority';
+        swalProps.title = 'Account disabled';
+        break;
+      case 'auth/wrong-password':
+        swalProps.description = 'Your password is incorrect';
+        swalProps.title = 'Wrong Password';
+        break;
+      default:
+        console.log('executing default');
+        swalProps.description = message;
+        swalProps.title = 'Error';
+        break;
+    }
+    swal(swalProps.title, swalProps.description, swalProps.type);
+  };
 }
