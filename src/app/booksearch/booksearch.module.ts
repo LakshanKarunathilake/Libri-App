c1fd4efa9d457598a70c0497b6328204a107c347
Unknown <manualsplash@gmail.com>
2019-04-05 22:57:09 +0530

    Create booksearch page

---
+++ src/app/booksearch/booksearch.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { BooksearchPage } from './booksearch.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BooksearchPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [BooksearchPage]
+})
+export class BooksearchPageModule {}
---
+++ src/app/booksearch/booksearch.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-booksearch',
+  templateUrl: './booksearch.page.html',
+  styleUrls: ['./booksearch.page.scss'],
+})
+export class BooksearchPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
