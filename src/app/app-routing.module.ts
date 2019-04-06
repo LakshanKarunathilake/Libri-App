2a4eeb2080fcbb2c9e30123c9db6a248dbf2dfa9
Unknown <manualsplash@gmail.com>
2019-04-06 22:52:45 +0530

    Remove sub routes from app router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },  { path: 'contact', loadChildren: './contact/contact.module#ContactPageModule' },
-  { path: 'booksearch', loadChildren: './booksearch/booksearch.module#BooksearchPageModule' },
-  { path: 'requestbook', loadChildren: './requestbook/requestbook.module#RequestbookPageModule' }
-
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' }
 ];
 
 @NgModule({
