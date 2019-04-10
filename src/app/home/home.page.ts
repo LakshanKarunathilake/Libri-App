4b09402d4ba3ac21b1628aa6d70980a8d1537d69
Unknown <manualsplash@gmail.com>
2019-04-10 12:27:14 +0530

    Change home page structure

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss']
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  pages = [
-    { title: 'Search Book', url: 'booksearch', icon: 'home' },
-    { title: 'Contact Us', url: 'contact', icon: 'home' },
-    { title: 'Request Book', url: 'requestbook', icon: 'home' }
-  ];
 
-  selectedPath = '';
+  constructor() { }
 
-  constructor(private router: Router) {
-    this.router.events.subscribe((event: RouterEvent) => {
-      if (event && event.url) {
-        this.selectedPath = event.url;
-        console.log('event.url', event.url);
-      }
-    });
+  ngOnInit() {
   }
 
-  isSelectedPath = url => {
-    return this.selectedPath.indexOf(url) >= 0;
-  };
-
-  ngOnInit() {}
 }
