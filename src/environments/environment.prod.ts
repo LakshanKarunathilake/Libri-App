da3884dd16f1985f623dcfbf9feac560af739f9d
Unknown <manualsplash@gmail.com>
2019-04-11 22:21:13 +0530

    Add firebase config to production environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD2ubSfF7xjL6cVKzO038c18MSf_dY1knU',
+    authDomain: 'libri-9746e.firebaseapp.com',
+    databaseURL: 'https://libri-9746e.firebaseio.com',
+    projectId: 'libri-9746e',
+    storageBucket: 'libri-9746e.appspot.com',
+    messagingSenderId: '893322827741'
+  }
 };
