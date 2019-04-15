da1aee41c9de521a812d5da9c929114915be8983
Unknown <manualsplash@gmail.com>
2019-04-15 22:28:52 +0530

    Change slider options
    Add multiple views for a single slide

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
+  sliderOptions = { slidesPerView: 2, spaceBetween: 10 };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
