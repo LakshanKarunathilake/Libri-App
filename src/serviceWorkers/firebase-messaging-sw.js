bc122fb386203a044d3a850091fe94c331dbf3d2
Unknown <manualsplash@gmail.com>
2019-04-16 22:40:19 +0530

    Update service worker firebase dependency versions

--- src/serviceWorkers/firebase-messaging-sw.js
+++ src/serviceWorkers/firebase-messaging-sw.js
@@ -1,16 +1,8 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
 firebase.initializeApp({
-  messagingSenderId:
-    'BEIwSvbugNbZGYqa7VvLTMH_Edzz5RwTjF7fwjZvIGzmIvLxcQm8uZQ0QxQ6Zk6Xl0W8Zgm3cxCEoiBGFQ0Aw9Y'
+  messagingSenderId: '893322827741'
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
