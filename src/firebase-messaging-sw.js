2441b254ba7138648e91cacac63f16e3874d6e13
Unknown <manualsplash@gmail.com>
2019-04-17 23:08:02 +0530

    Change service worker location

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,6 +2,11 @@ importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-messaging.js');
 
 firebase.initializeApp({
+  apiKey: 'AIzaSyD2ubSfF7xjL6cVKzO038c18MSf_dY1knU',
+  authDomain: 'libri-9746e.firebaseapp.com',
+  databaseURL: 'https://libri-9746e.firebaseio.com',
+  projectId: 'libri-9746e',
+  storageBucket: 'libri-9746e.appspot.com',
   messagingSenderId: '893322827741'
 });
 
