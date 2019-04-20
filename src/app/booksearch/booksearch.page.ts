caad9660c770158376dbca2741777771c1ac2a98
Unknown <manualsplash@gmail.com>
2019-04-20 16:08:41 +0530

    Modal open functionality

--- src/app/booksearch/booksearch.page.ts
+++ src/app/booksearch/booksearch.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { BookViewComponent } from '../components/book-view/book-view.component';
 
 @Component({
   selector: 'app-booksearch',
   templateUrl: './booksearch.page.html',
-  styleUrls: ['./booksearch.page.scss'],
+  styleUrls: ['./booksearch.page.scss']
 })
 export class BooksearchPage implements OnInit {
+  constructor(private modalController: ModalController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  bookPreview = async () => {
+    console.log('clicked');
+    const modal = await this.modalController.create({
+      component: BookViewComponent,
+      componentProps: {
+        bookName: 'dfdsfdsf',
+        id: 'dafsdfsd'
+      }
+    });
+    modal.present();
+  };
 }
