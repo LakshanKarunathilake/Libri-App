0f6a24cab99d74b5838f3e02e6cc13a5e6ec508f
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-02 20:10:46 +0530

    Delete contact page and create feedback page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
-  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
-  { path: 'account', loadChildren: './account/account.module#AccountPageModule' }
-
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' }
 ];
 
 @NgModule({
