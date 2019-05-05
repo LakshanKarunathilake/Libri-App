770b7cc8af261673a38ed7482f94ca68be898c5a
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-05 12:01:15 +0530

    Add basic stepper for the book transfer

--- src/app/transfer/transfer.module.ts
+++ src/app/transfer/transfer.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { TransferPage } from './transfer.page';
+import { MenuTitleComponentModule } from '../components/menu-title/menu-title.module';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 const routes: Routes = [
   {
@@ -19,7 +25,14 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MenuTitleComponentModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   declarations: [TransferPage]
 })
--- src/app/transfer/transfer.page.ts
+++ src/app/transfer/transfer.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.page.html',
-  styleUrls: ['./transfer.page.scss'],
+  styleUrls: ['./transfer.page.scss']
 })
 export class TransferPage implements OnInit {
-
-  constructor() { }
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  options = ['new feature', 'error/bug', 'improvement'];
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
-
 }
