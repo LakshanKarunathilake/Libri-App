6fb7efa8c3beda8386481661cf986bc90ae38c24
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-06 20:06:03 +0530

    Add success and error message

--- src/app/services/swal/swal.service.ts
+++ src/app/services/swal/swal.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import swal from 'sweetalert';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwalService {
+  constructor() {}
 
-  constructor() { }
+  viewSuccessMessage = (title: string, description: string) => {
+    swal(title, description, 'success');
+  };
+
+  viewErrorMessage = (title: string, description: string) => {
+    swal(title, description, 'error');
+  };
 }
