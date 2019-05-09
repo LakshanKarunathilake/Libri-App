2508ec3576918356456e67047c87e00722c9ee92
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-09 14:00:50 +0530

    Parameterize book view component

--- src/app/components/book-view/book-view.component.ts
+++ src/app/components/book-view/book-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -8,7 +8,7 @@ import { ModalController } from '@ionic/angular';
 })
 export class BookViewComponent implements OnInit {
   constructor(private modalController: ModalController) {}
-
+  @Input() book;
   ngOnInit() {}
 
   closeModal = () => {
