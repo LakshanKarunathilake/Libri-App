6425cf6f3f0fc33f3847a78f3d28fb622e58387d
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-09 13:33:07 +0530

    Rename book-view-desktop component to book-search-view

---
+++ src/app/components/book-search-view/book-search-view.component.ts
@@ -0,0 +1,28 @@
+import { BookViewComponent } from '../book-view/book-view.component';
+import { ModalController } from '@ionic/angular';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'book-search-view',
+  templateUrl: './book-search-view.component.html',
+  styleUrls: ['./book-search-view.component.scss']
+})
+export class BookSearchView implements OnInit {
+  @Input() book;
+  constructor(private modalController: ModalController) {}
+
+  ngOnInit() {}
+
+  bookPreview = async () => {
+    console.log('clicked');
+    const modal = await this.modalController.create({
+      component: BookViewComponent,
+      componentProps: {
+        bookName: 'dfdsfdsf',
+        id: 'dafsdfsd'
+      },
+      cssClass: 'book-view-modal'
+    });
+    modal.present();
+  };
+}
