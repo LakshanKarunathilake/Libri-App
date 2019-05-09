c85d89785b5324ccb9abddeb207b6092ad325c70
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-09 10:47:40 +0530

    Generate book-view-mobile component

---
+++ src/app/components/book-view-mobile/book-view-mobile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-view-mobile',
+  templateUrl: './book-view-mobile.component.html',
+  styleUrls: ['./book-view-mobile.component.scss'],
+})
+export class BookViewMobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
