908ae3cb0ce70daa8977d5ff160d8606f70c588e
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-15 20:37:24 +0530

    Change fcm call stratgey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      this.fcm.showMessages().subscribe();
+      this.fcm.showMessages();
     });
   }
 }
--- src/app/fcm.service.ts
+++ src/app/fcm.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AngularFireMessaging } from '@angular/fire/messaging';
 import { AngularFireFunctions } from '@angular/fire/functions';
-import { ToastController } from '@ionic/angular';
+import { ToastController, Platform } from '@ionic/angular';
 import { tap } from 'rxjs/operators';
-
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FcmService {
   token;
+  device;
 
   constructor(
     private afMessaging: AngularFireMessaging,
     private fun: AngularFireFunctions,
-    private toastController: ToastController
-  ) {}
+    private toastController: ToastController,
+    private platform: Platform
+  ) {
+    this.device = this.platform.is('cordova') ? 'cordova' : 'non-cordova';
+  }
 
   async makeToast(message) {
     const toast = await this.toastController.create({
@@ -31,28 +33,40 @@ export class FcmService {
 
   getPermission() {
     return new Promise((resolve, reject) => {
-      this.afMessaging.requestToken.subscribe(
-        token => {
-          console.log('Permission granted and token is ', token);
-          this.token = token;
-          resolve(token);
-        },
-        error => {
-          console.log('error occured when requesting permission');
-          console.error(error);
-          reject('error occured');
-        }
-      );
+      if (this.device !== 'cordova') {
+        this.afMessaging.requestToken.subscribe(
+          token => {
+            console.log('Permission granted and token is ', token);
+            this.token = token;
+            resolve(token);
+          },
+          error => {
+            console.log('error occured when requesting permission');
+            console.error(error);
+            reject('error occured');
+          }
+        );
+      } else {
+        // this.fm
+        //   .getToken()
+        //   .then(token => {
+        //     this.token = token;
+        //     swal('token recieved', token);
+        //     resolve(token);
+        //   })
+        //   .catch(error => {
+        //     swal('error', error);
+        //     reject(error);
+        //   });
+      }
     });
   }
 
-  showMessages(): Observable<any> {
-    return this.afMessaging.messages.pipe(
-      tap(msg => {
-        const body: any = (msg as any).notification.body;
-        this.makeToast(body);
-      })
-    );
+  showMessages() {
+    return this.afMessaging.messages.subscribe(msg => {
+      const body: any = (msg as any).notification.body;
+      this.makeToast(body);
+    });
   }
 
   sub(topic) {
