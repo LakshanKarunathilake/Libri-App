01a772ad9cb7e0b8b1968db421b49f82799f64d0
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-28 14:48:07 +0530

    Add book service saving functionality

--- src/app/components/book-view/book-view.component.ts
+++ src/app/components/book-view/book-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { BookService } from 'src/app/services/book/book.service';
 
 @Component({
   selector: 'app-book-view',
@@ -7,11 +8,21 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./book-view.component.scss']
 })
 export class BookViewComponent implements OnInit {
-  constructor(private modalController: ModalController) {}
+  constructor(private modalController: ModalController, private bookService: BookService) {}
   @Input() book;
   ngOnInit() {}
 
+  /**
+   * Closing the book view modal
+   */
   closeModal = () => {
     this.modalController.dismiss();
+  };
+
+  /**
+   * Adding book to the personal book shelf
+   */
+  saveBook = () => {
+    this.bookService.addBookToShelf(this.book);
   };
 }
