e1861aeea045bda82ce9a837908d0de2ca1e80e4
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-28 14:46:39 +0530

    Add view for account tab menu

--- src/app/components/account-tab-menu/account-tab-menu.component.ts
+++ src/app/components/account-tab-menu/account-tab-menu.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-account-tab-menu',
+  selector: 'account-tab-menu',
   templateUrl: './account-tab-menu.component.html',
-  styleUrls: ['./account-tab-menu.component.scss'],
+  styleUrls: ['./account-tab-menu.component.scss']
 })
 export class AccountTabMenuComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
