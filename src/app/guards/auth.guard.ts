3d444fcb02f28d85348e555c854b10a7820e5dbc
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-31 23:05:23 +0530

    Update card view for borrowings

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,15 @@
+import { AngularFireAuth } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { AlertController } from '@ionic/angular';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard {}
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuard implements CanActivate {
+//   CanActivate(route: ActivatedRouteSnapshot) {
+//     const currentUser = this.afa.currentUser;
+//   }
+// }
