35e7595b9dbf664017dd887bee84f7d9adbcfdc2
Lakshan Karunathilake <manualsplash@gmail.com>
2019-06-12 09:17:28 +0530

    Add QR code scanning facility

--- src/app/transfer/transfer.page.ts
+++ src/app/transfer/transfer.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { SwalService } from '../services/swal/swal.service';
 
 @Component({
   selector: 'app-transfer',
@@ -10,7 +12,11 @@ export class TransferPage implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   options = ['new feature', 'error/bug', 'improvement'];
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(
+    private _formBuilder: FormBuilder,
+    private scanner: BarcodeScanner,
+    private swal: SwalService
+  ) {}
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
@@ -23,5 +29,20 @@ export class TransferPage implements OnInit {
 
   getTitle = () => {
     return this.firstFormGroup.controls['firstCtrl'].value || 'Value not found';
+  };
+
+  scanForQR = () => {
+    this.scanner
+      .scan()
+      .then(data => {
+        alert(data.text);
+      })
+      .catch(error => {
+        console.log('error occured while scanning QR', error);
+        this.swal.viewErrorMessage(
+          'Error',
+          'You have to be on your iOS or android version inorder to access this service'
+        );
+      });
   };
 }
