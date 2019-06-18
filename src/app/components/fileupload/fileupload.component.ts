1cdbf1c631a3fe9e6fee78c5520d44999a895ba3
Desktop <manualsplash@gmail.com>
2019-06-18 22:50:37 +0530

    Implement the file component functionality

--- src/app/components/fileupload/fileupload.component.ts
+++ src/app/components/fileupload/fileupload.component.ts
@@ -1,14 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fileupload',
   templateUrl: './fileupload.component.html',
-  styleUrls: ['./fileupload.component.scss'],
+  styleUrls: ['./fileupload.component.scss']
 })
 export class FileuploadComponent implements OnInit {
+  // Main task
+  task: AngularFireUploadTask;
 
-  constructor() { }
+  // Progress monitoring
+  percentage: Observable<number>;
 
-  ngOnInit() {}
+  snapshot: Observable<any>;
 
+  // Download URL
+  downloadURL: Observable<string>;
+
+  // State for dropzone CSS toggling
+  isHovering: boolean;
+
+  constructor(private storage: AngularFireStorage, private db: AngularFirestore) {}
+  ngOnInit(): void {}
+
+  toggleHover(event: boolean) {
+    this.isHovering = event;
+  }
+
+  startUpload(event: FileList) {
+    // The File object
+    const file = event.item(0);
+
+    // Client-side validation example
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('unsupported file type :( ');
+      return;
+    }
+
+    // The storage path
+    const path = `test/${new Date().getTime()}_${file.name}`;
+
+    // Totally optional metadata
+    const customMetadata = { app: 'My AngularFire-powered PWA!' };
+
+    // The main task
+    this.task = this.storage.upload(path, file, { customMetadata });
+
+    // Progress monitoring
+    this.percentage = this.task.percentageChanges();
+    this.snapshot = this.task.snapshotChanges();
+
+    // The file's download URL
+    this.snapshot
+      .pipe(
+        tap(snap => {
+          if (snap.bytesTransferred === snap.totalBytes) {
+            // Update firestore on completion
+            this.db.collection('photos').add({ path, size: snap.totalBytes });
+          }
+        }),
+        finalize(() => (this.downloadURL = this.storage.ref(path).getDownloadURL()))
+      )
+      .subscribe();
+  }
+
+  // Determines if the upload task is active
+  isActive(snapshot) {
+    return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes;
+  }
 }
