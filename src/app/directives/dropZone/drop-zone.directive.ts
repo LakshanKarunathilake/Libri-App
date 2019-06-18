0b7ec918a7819068682521169e17183bd95cc9db
Desktop <manualsplash@gmail.com>
2019-06-18 22:50:51 +0530

    Update dropzone directive

--- src/app/directives/dropZone/drop-zone.directive.ts
+++ src/app/directives/dropZone/drop-zone.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropZone]'
 })
 export class DropZoneDirective {
+  @Output() dropped = new EventEmitter<FileList>();
+  @Output() hovered = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor() {}
 
+  @HostListener('drop', ['$event'])
+  onDrop($event) {
+    $event.preventDefault();
+    this.dropped.emit($event.dataTransfer.files);
+    this.hovered.emit(false);
+  }
+
+  @HostListener('dragover', ['$event'])
+  onDragOver($event) {
+    $event.preventDefault();
+    this.hovered.emit(true);
+  }
+
+  @HostListener('dragleave', ['$event'])
+  onDragLeave($event) {
+    $event.preventDefault();
+    this.hovered.emit(false);
+  }
 }
