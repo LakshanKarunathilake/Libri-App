b1fc83a04c1072367be45de7ba8b425470eb1622
Desktop <manualsplash@gmail.com>
2019-06-20 23:49:16 +0530

    Add file upload service

--- src/app/requestbook/requestbook.module.ts
+++ src/app/requestbook/requestbook.module.ts
@@ -11,6 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FileuploadComponent } from '../components/fileupload/fileupload.component';
 import { FileSizePipe } from '../pipes/file-size.pipe';
 import { DropZoneDirective } from '../directives/dropZone/drop-zone.directive';
+import { FileUploadService } from '../services/file-upload.service';
 
 const routes: Routes = [
   {
@@ -29,7 +30,13 @@ const routes: Routes = [
     MenuTitleComponentModule,
     ReactiveFormsModule
   ],
-  declarations: [RequestbookPage, DropZoneDirective, FileuploadComponent, FileSizePipe],
+  declarations: [
+    RequestbookPage,
+    DropZoneDirective,
+    FileuploadComponent,
+    FileSizePipe,
+    FileUploadService
+  ],
   entryComponents: [FileuploadComponent]
 })
 export class RequestbookPageModule {}
