44ef747913d7e7068323a7115b1de66e01b903ac
Desktop <manualsplash@gmail.com>
2019-06-21 00:04:58 +0530

    Clear functionalities of component

--- src/app/components/fileupload/fileupload.component.ts
+++ src/app/components/fileupload/fileupload.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireUploadTask } from '@angular/fire/storage';
 import { Observable } from 'rxjs';
-import { finalize, tap } from 'rxjs/operators';
 import { FileUploadService } from 'src/app/services/file-upload.service';
 
 @Component({
@@ -28,11 +26,7 @@ export class FileuploadComponent implements OnInit {
   // State for disable the file drop
   isDropped = false;
 
-  constructor(
-    private storage: AngularFireStorage,
-    private db: AngularFirestore,
-    private uploader: FileUploadService
-  ) {}
+  constructor(private uploader: FileUploadService) {}
   ngOnInit(): void {}
 
   toggleHover(event: boolean) {
@@ -43,37 +37,14 @@ export class FileuploadComponent implements OnInit {
     this.isDropped = true;
     // The File object
     const file = event.item(0);
-
-    // Client-side validation example
-    if (file.type.split('/')[0] !== 'image') {
-      console.error('unsupported file type :( ');
-      return;
-    }
-
-    // The storage path
-    const path = `test/${new Date().getTime()}_${file.name}`;
-
-    // Totally optional metadata
-    const customMetadata = { app: 'My AngularFire-powered PWA!' };
-
-    // The main task
-    this.task = this.storage.upload(path, file, { customMetadata });
-
-    // Progress monitoring
-    this.percentage = this.task.percentageChanges();
-    this.snapshot = this.task.snapshotChanges();
-
-    // The file's download URL
-    this.snapshot
-      .pipe(
-        tap(snap => {
-          if (snap.bytesTransferred === snap.totalBytes) {
-            // Update firestore on completion
-            this.db.collection('photos').add({ path, size: snap.totalBytes });
-          }
-        }),
-        finalize(() => (this.downloadURL = this.storage.ref(path).getDownloadURL()))
-      )
-      .subscribe();
+    this.uploader.uploadFile(file, this.setDownloadbaleURL, this.setTask);
   }
+  setTask = (task: AngularFireUploadTask) => {
+    this.snapshot = task.snapshotChanges();
+    this.percentage = task.percentageChanges();
+  };
+
+  setDownloadbaleURL = url => {
+    this.downloadURL = url;
+  };
 }
