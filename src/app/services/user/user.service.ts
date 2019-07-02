b55a9f830ff69dc5c2e2389e4166544f8580daf4
Desktop <manualsplash@gmail.com>
2019-07-02 23:19:26 +0530

    Change method name and callable function name

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -59,8 +59,8 @@ export class UserService {
    * You can obtain all the transactional data relavant to the user by calling this method
    * The data will include the borrowings, overdues and penalties
    */
-  getUserTransactions = () => {
+  getUserBorrowings = () => {
     const { uid } = this.getCurrentUser();
-    return this.aff.functions.httpsCallable('personalTransacitons')({ uid });
+    return this.aff.functions.httpsCallable('getUserBorrowings')({ uid });
   };
 }
