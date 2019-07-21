d9e523a91126473e101d06ef67c95811eaba6b6b
Desktop <manualsplash@gmail.com>
2019-07-21 22:29:36 +0530

    Try Firebase auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
-  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' }
+  {
+    path: 'menu',
+    loadChildren: './menu/menu.module#MenuPageModule',
+    canActivate: [AngularFireAuthGuard]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AngularFireAuthGuard]
 })
 export class AppRoutingModule {}
