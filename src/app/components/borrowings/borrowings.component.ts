5d91e709289d7837e58c8959045739f9489774d7
Lakshan Karunathilake <manualsplash@gmail.com>
2019-07-28 22:30:57 +0530

    Inject user service

--- src/app/components/borrowings/borrowings.component.ts
+++ src/app/components/borrowings/borrowings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-borrowings',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BorrowingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {}
 
