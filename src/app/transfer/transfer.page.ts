bbc3d5aecee8deca9f768e4f33bad17079a11c30
Desktop <manualsplash@gmail.com>
2019-08-11 22:10:07 +0530

    Remove form validation

--- src/app/transfer/transfer.page.ts
+++ src/app/transfer/transfer.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { SwalService } from '../services/swal/swal.service';
 import { BookService } from '../services/book/book.service';
@@ -11,25 +10,14 @@ import { MatStepper } from '@angular/material/stepper';
   styleUrls: ['./transfer.page.scss']
 })
 export class TransferPage implements OnInit {
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  options = ['new feature', 'error/bug', 'improvement'];
   @ViewChild('stepper') stepper: MatStepper;
   constructor(
-    private _formBuilder: FormBuilder,
     private scanner: BarcodeScanner,
     private swal: SwalService,
     private bookService: BookService
   ) {}
 
-  ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-  }
+  ngOnInit() {}
 
   getTitle = () => {
     return this.firstFormGroup.controls['firstCtrl'].value || 'Value not found';
