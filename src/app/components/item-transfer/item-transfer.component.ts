743fa6c2d5735611674802b6f5d5da364b4b9f08
Lakshan Karunathilake <manualsplash@gmail.com>
2019-08-24 12:56:09 +0530

    Get borrowing details of individual book

--- src/app/components/item-transfer/item-transfer.component.ts
+++ src/app/components/item-transfer/item-transfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Borrowing } from 'src/app/models/Borrowings';
 
 @Component({
   selector: 'item-transfer',
@@ -7,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ItemTransferComponent implements OnInit {
   @Input() moveHeader: Function;
+  @Input() borrowDetails: Borrowing;
   constructor() {}
 
   ngOnInit() {}
