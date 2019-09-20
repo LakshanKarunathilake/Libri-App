fea6af75139a736ccfa844083436a0650769c5ac
Lakshan Karunathilake <manualsplash@gmail.com>
2019-09-20 13:31:40 +0530

    Add validation for existing user

--- src/app/services/custom-validator/custom-validator.service.ts
+++ src/app/services/custom-validator/custom-validator.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+import { UserService } from '../user/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomValidatorService {
-  constructor() {}
+  constructor(private userService: UserService) {}
+
+  libraryIdValidator = (control: AbstractControl): { [key: string]: any } | null => {
+    const id = control.value;
+    const data = this.userService.isUserRegistered(id);
+    if (!data) {
+      return null;
+    } else {
+      return { userRegistered: true };
+    }
+  };
 }
