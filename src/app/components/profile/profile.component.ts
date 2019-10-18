8393428702ed2efb6fde0e2d8861dd6acb69f9d4
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-18 20:59:29 +0530

    Add password reset functionality

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -9,6 +9,13 @@ export class ProfileComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  // Sending a password reset email
+  resetPassword = () => {
+    this.swal.displayConfirmation(
+      'Confrimation',
+      'You are about to reset your password please confirm your request',
+      () => this.user.resetPassword()
+    );
+  };
 
 }
