05a676d773d2364ca17a9243e2a9cc720439a54b
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-18 21:01:16 +0530

    Get topics for FCM

--- src/app/services/overall/overall.service.ts
+++ src/app/services/overall/overall.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OverallService {
+  constructor(private afs: AngularFirestore) {}
 
-  constructor() { }
+  /**
+   * Return an Observable of topics
+   * Method will query firebase documents created by the Library Admin and query for the notice topics
+   */
+  getNoticeTopics = () => {
+    return this.afs
+      .collection('admin')
+      .doc('notices')
+      .collection<Topic>('topics')
+      .valueChanges();
+  };
 }
