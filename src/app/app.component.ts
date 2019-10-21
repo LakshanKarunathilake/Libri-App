7f8ce36a530b28d5b0c0ac6ac961cafb27c0008c
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-21 23:38:14 +0530

    Add splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { FcmService } from './fcm.service';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  showSplash = true; // <-- show animation
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private fcm: FcmService
-  ) {
+  ) {}
+
+  ngOnInit(): void {
     this.initializeApp();
   }
 
@@ -23,6 +27,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      timer(10000).subscribe(() => (this.showSplash = false));
       this.fcm.getPermission().then(() => this.fcm.showMessages());
     });
   }
