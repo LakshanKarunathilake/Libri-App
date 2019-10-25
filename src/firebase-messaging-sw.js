62eb8db22f7a53adcae9c86d7d84ed681270e278
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-25 19:43:35 +0530

    Update service worker

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,6 +1,11 @@
-importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-messaging.js');
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js');
 
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
 firebase.initializeApp({
   apiKey: 'AIzaSyDuPVBd60jVbPw1BRefZsTkWb2AXZsfzNk',
   authDomain: 'libri-238805.firebaseapp.com',
@@ -8,7 +13,10 @@ firebase.initializeApp({
   projectId: 'libri-238805',
   storageBucket: 'libri-238805.appspot.com',
   messagingSenderId: '696789073783',
-  appId: '1:696789073783:web:3579e974bc60674c'
+  appId: '1:696789073783:web:3579e974bc60674c',
+  measurementId: 'G-D5R7Q086CT'
 });
 
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
