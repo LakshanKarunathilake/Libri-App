a5cfee66f439e3fa38bc196b90de2a819fec6bc7
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-26 23:09:17 +0530

    Add tests for penalty component
    Dummies included

--- src/app/components/penalty/penalty.component.spec.ts
+++ src/app/components/penalty/penalty.component.spec.ts
@@ -1,6 +1,11 @@
+import { AngularFireAuth } from '@angular/fire/auth';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PenaltyComponent } from './penalty.component';
+import { AngularFireFunctionsStub, UserServiceStub, SwalServiceStub } from 'src/app/Stubs';
+import { SwalService } from 'src/app/services/swal/swal.service';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { UserService } from 'src/app/services/user/user.service';
 
 describe('PenaltyComponent', () => {
   let component: PenaltyComponent;
@@ -9,6 +14,11 @@ describe('PenaltyComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PenaltyComponent],
+      providers: [
+        { provide: AngularFireFunctions, useClass: AngularFireFunctionsStub },
+        { provide: UserService, useClass: UserServiceStub },
+        { provide: SwalService, useclass: SwalServiceStub }
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
@@ -16,10 +26,9 @@ describe('PenaltyComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PenaltyComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create penalty component with services', () => {
     expect(component).toBeTruthy();
   });
 });
