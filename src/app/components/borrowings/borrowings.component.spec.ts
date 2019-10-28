b728ac0843a9d5fa47eaa6857e83ae8c205a57bf
Lakshan Karunathilake <manualsplash@gmail.com>
2019-10-28 15:40:08 +0530

    Tests for borrowings

--- src/app/components/borrowings/borrowings.component.spec.ts
+++ src/app/components/borrowings/borrowings.component.spec.ts
@@ -1,14 +1,17 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BorrowingsComponent } from './borrowings.component';
+import { UserService } from 'src/app/services/user/user.service';
+import { UserServiceStub } from 'src/app/Stubs';
 
-xdescribe('BorrowingsComponent', () => {
+describe('BorrowingsComponent', () => {
   let component: BorrowingsComponent;
   let fixture: ComponentFixture<BorrowingsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [BorrowingsComponent],
+      providers: [{ provide: UserService, useClass: UserServiceStub }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
@@ -19,7 +22,11 @@ xdescribe('BorrowingsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create borrowings component view', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return user borrowings after loading', () => {
+    expect(component.overdues).not.toBeNull();
   });
 });
